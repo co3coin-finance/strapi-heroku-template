2c75b293ba52d895746c0c70f246a49753bf83af
Co3coin <84360815+co3coin@users.noreply.github.com>
2021-05-27 21:34:49 -0300

    Update database.js

--- config/env/develop/database.js
+++ config/env/develop/database.js
@@ -1,25 +1,19 @@
-const parse = require('pg-connection-string').parse;
-const config = parse(process.env.DATABASE_URL);
-
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
-  connections: {
-    default: {
-      connector: 'bookshelf',
-      settings: {
-        client: 'postgres',
-        host: config.host,
-        port: config.port,
-        database: config.database,
-        username: config.user,
-        password: config.password,
-        ssl: {
-          rejectUnauthorized: false,
-        },
-      },
-      options: {
-        ssl: true,
-      },
+defaultConnection: "default",
+connections: {
+  default: {
+    connector: "bookshelf",
+    settings: {
+      client: "postgres",
+      host: env("DATABASE_HOST", "localhost"),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "strapi"),
+      username: env("DATABASE_USERNAME", "strapi"),
+      password: env("DATABASE_PASSWORD", "strapi"),
+      schema: "public",
+      ssl: { rejectUnauthorized: false }
     },
-  },
+    options: {}
+  }
+}
 });
